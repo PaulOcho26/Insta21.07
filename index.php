94208c360c349329cdaea425d72fa02e9cffdadc
PaulOcho26 <paul.bourlier2026@gmail.com>
2025-07-21 13:42:48 +0200

    index.php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="upload-photo.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="picture">
+        <button>Envoyer</button>
+    </form>
 </body>
 </html>
